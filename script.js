c53b9f86ed5479edf18c10980b4f5efd3246e524
Julio Postigo <jpostigo89@gmail.com>
2021-09-23 21:02:23 -0300

    last from class2

--- script.js
+++ script.js
@@ -18,6 +18,15 @@ let data = {
 };
 const sendBtn = document.querySelector("#sendBtn");
 
+//se crean los "lectores" de html para relacionarlos con una variable que podamos usar en JS
+
+const contenedorPersonas = document.querySelector("#contenedorPersonas");
+const nombreCompleto = document.querySelector("#nombreCompleto");
+const edadPersona = document.querySelector("#edadPersona");
+const generoPersona = document.querySelector("#generoPersona");
+const telefonoPersona = document.querySelector("#telefonoPersona");
+const compañiaTelefono = document.querySelector("#compañiaTelefono");
+const suscripcionPersona = document.querySelector("#suscripcionPersona");
 const form = document.querySelector("#form");
 
 const handleSendBtn = (e) => {
@@ -32,11 +41,63 @@ const handleSendBtn = (e) => {
     email: form.correo.value,
     suscripcionNews: form.suscripcion.value,
   };
+  //... significa: lo que ya tenia data
   data = { ...data, personas: [...data.personas, nuevaPersona] };
 
   form.reset(); //para resetear formulario cada vez q se añade alguien
 
-  console.log(data.personas);
+  //para cargar datos de las personas en el html, antes se crearon las constantes de los id de listado de personas,
+  //que esta en el contacto.html
+  nombreCompleto.innerText = `${nuevaPersona.nombre} ${nuevaPersona.apellido}`;
+  edadPersona.innerText = nuevaPersona.edad;
+  generoPersona.innerText = nuevaPersona.genero;
+  telefonoPersona.innerText = nuevaPersona.telefono;
+  compañiaTelefono.innerText = nuevaPersona.compañiaTelefono;
+  suscripcionPersona.innerText = nuevaPersona.suscripcionNews;
+
+  //llama a la otra funcion para agregar las personas
+  addNewPerson(nuevaPersona);
+
+  //console.log(data.personas);
+};
+
+const addNewPerson = (nuevaPersona) => {
+  //para que las personas aparezcan todos en el html, y no se borren al cargar otra persona
+  const elementoNombre = document.createElement("h3");
+  //ahora agregamos clase para poder darle estilos con css, se trabaja con - no con camelCase
+  elementoNombre.classList.add("nombre-persona");
+  const elementoEdad = document.createElement("h3");
+  elementoEdad.classList.add("edad-persona");
+  const elementoGenero = document.createElement("h3");
+  elementoGenero.classList.add("genero-persona");
+  const elementoTelefono = document.createElement("h4");
+  elementoTelefono.classList.add("telefono-persona");
+  const elementoCompañiaTel = document.createElement("h4");
+  elementoCompañiaTel.classList.add("compañiaTel-persona");
+  const elementoSuscripto = document.createElement("h4");
+  elementoSuscripto.classList.add("suscripcion-persona");
+
+  //para que en inspeccionar elementos, se vea cada persona dentro de un div
+  const contenedorPersona = document.createElement("div");
+  contenedorPersona.classList.add("persona");
+
+  //populamos los elementos con los datos correspondientes, asi se nos van agregando uno debajo de otro
+  elementoNombre.innerText = `${nuevaPersona.nombre} ${nuevaPersona.apellido}`;
+  elementoEdad.innerText = nuevaPersona.edad;
+  elementoGenero.innerText = nuevaPersona.genero;
+  elementoTelefono.innerText = nuevaPersona.telefono;
+  elementoCompañiaTel.innerText = nuevaPersona.compañiaTelefono;
+  elementoSuscripto.innerText = nuevaPersona.suscripcionNews;
+
+  //se añaden los elementos al html, y luego se agrega al contenedor gral de personas
+  contenedorPersona.appendChild(elementoNombre);
+  contenedorPersona.appendChild(elementoEdad);
+  contenedorPersona.appendChild(elementoGenero);
+  contenedorPersona.appendChild(elementoTelefono);
+  contenedorPersona.appendChild(elementoCompañiaTel);
+  contenedorPersona.appendChild(elementoSuscripto);
+
+  contenedorPersonas.appendChild(contenedorPersona);
 };
 
 sendBtn.addEventListener("click", handleSendBtn);
@@ -46,5 +107,4 @@ sendBtn.addEventListener("click", handleSendBtn);
 const title = document.createElement("h1");
 title.innerText = "Soy un titulo";
 
-const contenedorPersonas = document.querySelector("#contenedorPersonas");
 contenedorPersonas.appendChild(title);
